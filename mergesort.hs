3aacf8a8c9554ae0d2f4be03e32cd70d1f977dd5
Sam Makman <sammakman@gmail.com>
2016-11-16 10:07:30 -0800

    fixed mergesort bug

--- mergesort.hs
+++ mergesort.hs
@@ -1,15 +1,18 @@
 mergeSort [] = error "Cannot Sort An Empty List"
 mergeSort [x] = [x]
-mergeSort xs = merge (mergeSort (take (length xs `div` 2) xs )) (mergeSort ( drop (length xs `div` 2) xs ))
+mergeSort xs = merge (mergeSort (take half xs )) (mergeSort ( drop half xs ))
+			where half = length xs `div` 2
 
 
 merge [] [] = [] 
+merge [x] [] = [x]
+merge [] [y] = [y]
 merge [x] (y:ys)
-	| x > y = y : x : ys 
-	| otherwise = x : y : ys 
+	| x  < y = x : y : ys 
+	| otherwise = y : merge [x]  ys 
 merge (x:xs) [y] 
 	| x > y = y : x : xs 
-	| otherwise = x : y : xs 
+	| otherwise = x : merge [y]  xs 
 merge (x:xs) (y:ys)
 	| x > y = y : merge (x:xs) ys 
 	| otherwise = x : merge xs (y:ys)
